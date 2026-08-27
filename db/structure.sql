e9b83134836027500a5bb1bdd8a7db724a2a4bd5
dannstein <nunessteindaniel@gmail.com>
2026-08-27 02:05:22 -0300

    Fix citext structure dump

--- db/structure.sql
+++ db/structure.sql
@@ -21,7 +21,6 @@ CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;
 -- Name: EXTENSION citext; Type: COMMENT; Schema: -; Owner: -
 --
 
-COMMENT ON EXTENSION citext IS 'data type for case-insensitive character strings';
 
 
 --
